4da66d4648271eac9fa92c3dd776a6d791382344
jack sparrow <1755610380@qq.com>
2018-04-03 20:15:23 +0800

    update consul config

---
+++ cloud-config-client/src/main/java/com/galaxy/config/client/ConfigClientApplication.java
@@ -0,0 +1,15 @@
+package com.galaxy.config.client;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by wangpeng on 2018/4/2.
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
